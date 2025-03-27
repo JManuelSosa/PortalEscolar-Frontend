0666e3c11241fd3b691240855a030959801733fa
JManuelSosa <163345575+JManuelSosa@users.noreply.github.com>
2025-03-27 13:14:33 -0600

    Agregado el header

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,3 +1,8 @@
+import { Col, Row } from "antd";
+import CardContent from "../Components/CardContent";
+
+import '../CSS/HomeView.css';
+
 
 export default function HomeView(){
 
@@ -5,7 +10,13 @@ export default function HomeView(){
     return(
         <>
             <h1 className="HomeTittle">Bienvenido {Usuario} </h1>
-            
+            <Row className="MainOpcion">
+                <Col sm={24} md={8}>
+                    <CardContent></CardContent>
+                </Col>
+                <Col sm={24} md={8}></Col>
+                <Col sm={24} md={8}></Col>
+            </Row>
         </>
     );
     
