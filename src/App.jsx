cd1dc06686504a7bce588437e21f3226f45b52aa
JManuelSosa <163345575+JManuelSosa@users.noreply.github.com>
2025-04-01 02:34:33 -0600

    Revert "Merge pull request #10 from JManuelSosa/Gilda"
    This reverts commit 8ac0cdeb8b85a550d8b3eb79e1158e97b44a1260, reversing
    changes made to 48a21206db2778e39bbc5a41d66b8e964bf0be3f.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import { CollapsedProvider } from './Context/CollapseContext';
 //Views
 import HomeView from './Views/HomeView';
-
 import Prueba from './Components/Prueba';
 import DivisionView from './Views/DivisionView';
 
@@ -13,17 +12,10 @@ import HeaderLayout from './Layout/HeaderLayout';
 
 //AntDesign
 import { Layout } from 'antd';
-const { Content, Footer } = Layout;
+const {Content, Footer } = Layout;
 
 //Css 
 import '../src/CSS/App.css';
-
-
-import { AuthProvider } from "./Context/AuthContext";
-import Login from './Components/Login/Login';
-import SignUp from './Components/SignUp/SignUp';
-
-
 
 const headerStyle = {
 
@@ -33,7 +25,7 @@ const footerStyle = {
   textAlign: 'center',
   color: '#fff',
   backgroundColor: '#4096ff',
-  height: 12,
+  height:12,
 };
 
 function App() {
@@ -41,26 +33,22 @@ function App() {
   const escuela = "UTM"
   return (
     <>
-      <Layout>
-        <CollapsedProvider>
-          <SiderNav />
-        </CollapsedProvider>
         <Layout>
-          <HeaderLayout></HeaderLayout>
-          <Content className='Content'>
-            <AuthProvider>
-              <Routes>
-                <Route path='/' element={<HomeView />} />
-                <Route path='/login' element={<Login />} />
-                <Route path="/sing-up" element={<SignUp />} />
-              </Routes>
-            </AuthProvider>
-          </Content>
-          <Footer style={footerStyle}>
-            Hola Footer
-          </Footer>
+          <CollapsedProvider>
+            <SiderNav/>
+          </CollapsedProvider>
+          <Layout>
+            <HeaderLayout></HeaderLayout>
+            <Content className='Content'>
+                <Routes>
+                  <Route path='/' element={<DivisionView/>}/>
+                </Routes>
+            </Content>
+            <Footer style={footerStyle}>
+              Hola Footer
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
     </>
   )
 }
--- src/Components/Registro.jsx
+++ src/Components/Registro.jsx
@@ -1,35 +1,35 @@
 import Registro from './Registro'
 import './Registro.css';
 
-export default function Registro() {
+export default function Registro(){
 
-    <div className='registro-container'>
-        <div className='registro-box'>
-            <h3>Registrate</h3>
-            <form action="">
-                <div className='input-registro'>
-                    <i class="fa-solid fa-user"></i>
-                    <input type="text" placeholder="E-mail" />
-                </div>
-
-                <div className='input-registro2'>
-                    <i class="fas fa-lock"></i>{/*<!-- esto el icono de candado-->*/}
-                    <input type="" placeholder="Contraseña" />
-                    <span class="eye-icon" onclick="togglePassword()" />
-
-                </div>
-                <div class="input-group2">
-                    <i class="fas fa-lock"></i>{/*<!-- esto el icono de candado-->*/}
-                    <input type="" placeholder="Contraseña" />
-                    <span class="eye-icon" onclick="togglePassword()" />
-
-                </div>
-                <div class="boton ">
-                    <button type="submit">Iniciar Sesión </button>
-                </div>
-            </form>
+<div className='registro-container'>
+    <div className='registro-box'>
+    <h3>Registrate</h3>
+    <form action="">
+        <div className='input-registro'>
+        <i class="fa-solid fa-user"></i>
+        <input type="text" placeholder="E-mail"/>
         </div>
+
+        <div className='input-registro2'>
+        <i class="fas fa-lock"></i>{/*<!-- esto el icono de candado-->*/}
+                    <input type="" placeholder="Contraseña"/>
+                    <span class="eye-icon" onclick="togglePassword()"/>
+
+        </div>
+        <div class="input-group2">
+                    <i class="fas fa-lock"></i>{/*<!-- esto el icono de candado-->*/}
+                    <input type="" placeholder="Contraseña"/>
+                    <span class="eye-icon" onclick="togglePassword()"/>
+                       
+              </div>
+              <div class="boton ">
+               <button type="submit">Iniciar Sesión </button>
+                  </div>
+    </form>
     </div>
+</div>
 
 
 }
